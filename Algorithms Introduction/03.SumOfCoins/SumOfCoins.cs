c7728f978be09d8e669abee58b1d643a7fb36145
Zlatomir9 <71620439+Zlatomir9@users.noreply.github.com>
2020-10-26 23:10:39 +0200

    Algorithms Exercises

---
+++ Algorithms Introduction/03.SumOfCoins/SumOfCoins.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SumOfCoins
+{
+    public static void Main(string[] args)
+    {
+        var availableCoins = new[] { 1, 2, 5, 10, 20, 50 };
+        var targetSum = 923;
+
+        var selectedCoins = ChooseCoins(availableCoins, targetSum);
+
+        Console.WriteLine($"Number of coins to take: {selectedCoins.Values.Sum()}");
+        foreach (var selectedCoin in selectedCoins)
+        {
+            Console.WriteLine($"{selectedCoin.Value} coin(s) with value {selectedCoin.Key}");
+        }
+    }
+
+    public static Dictionary<int, int> ChooseCoins(IList<int> coins, int targetSum)
+    {
+        Dictionary<int, int> returnedCoins = new Dictionary<int, int>();
+       
+        coins = coins.OrderByDescending(x => x).ToArray();
+        int change = 0;
+
+        while (change < targetSum)
+        {
+            for (int i = 0; i < coins.Count; i++)
+            {
+                if (change + coins[i] <= targetSum)
+                {
+                    change += coins[i];
+                    if (!returnedCoins.ContainsKey(coins[i]))
+                    {
+                        returnedCoins.Add(coins[i], 1);
+                    }
+                    else
+                    {
+                        returnedCoins[coins[i]]++;
+                    }
+                    break;
+                }
+            }
+        }
+
+        return returnedCoins;
+    }
+}
---
+++ Algorithms Introduction/BinarySearch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BinarySearch(arr, num, 0, arr.Length));
+        }
+
+        private static int BinarySearch(int[] arr, int num, int start, int end)
+        {
+            if (start > end)
+            {
+                return -1;
+            }
+
+            int mid = (start + end) / 2;
+
+            if (num < arr[mid])
+            {
+                return BinarySearch(arr, num, start, mid - 1);
+            }
+            else if (num > arr[mid])
+            {
+                return BinarySearch(arr, num, mid + 1, arr.Length);
+            }
+            else
+            {
+                return mid;
+            }
+        }
+    }
+}
