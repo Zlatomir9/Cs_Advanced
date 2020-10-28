fde8996613f04dacdd16d915c80a304eb5504b0f
Zlatomir9 <71620439+Zlatomir9@users.noreply.github.com>
2020-10-28 23:19:08 +0200

    Algorithms Exercises

---
+++ Algorithms Introduction/MergeSort/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<int> sorted = MergeSort(arr.ToList());
+            Console.WriteLine(String.Join(' ', sorted));
+        }
+
+        private static List<int> MergeSort(List<int> list)
+        {
+            if (list.Count <= 1)
+            {
+                return list;
+            }
+
+            int middle = list.Count / 2;
+            List<int> left = MergeSort(list.GetRange(0, middle));
+            List<int> right = MergeSort(list.GetRange(middle, list.Count - middle));
+
+            return CombineArrays(left, right);
+        }
+
+        private static List<int> CombineArrays(List<int> left, List<int> right) 
+        {
+            List<int> sorted = new List<int>();
+            int leftIndex = 0;
+            int rightIndex = 0;
+
+            while (leftIndex < left.Count && rightIndex < right.Count)
+            {
+                if (left[leftIndex] < right[rightIndex])
+                {
+                    sorted.Add(left[leftIndex]);
+                    leftIndex++;
+                }
+                else
+                {
+                    sorted.Add(right[rightIndex]);
+                    rightIndex++;
+                }
+            }
+
+            for (int i = leftIndex; i < left.Count; i++)
+            {
+                sorted.Add(left[i]);
+            }
+
+            for (int i = rightIndex; i < right.Count; i++)
+            {
+                sorted.Add(right[i]);
+            }
+
+            return sorted;
+        }
+    }
+}
---
+++ Algorithms Introduction/QuickSort/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var list = arr.ToList();
+            QuickSort(list, 0, list.Count - 1);
+            Console.WriteLine(String.Join(' ', list));
+        }
+
+        static void QuickSort(List<int> arr, int left, int right)
+        {
+            if (left < right)
+            {
+                var partitionIndex = Partition(arr, left, right);
+                QuickSort(arr, left, partitionIndex);
+                QuickSort(arr, partitionIndex + 1, right);
+            }
+        }
+
+        static int Partition(List<int> arr, int left, int right)
+        {
+            int pivot = arr[(left + right) / 2];
+            int i = left - 1;
+            int j = right + 1;
+
+            while (true)
+            {
+                do
+                {
+                    i++;
+                }
+                while (arr[i] < pivot);
+                do
+                {
+                    j--;
+                }
+                while (arr[j] > pivot);
+
+                if (i >= j)
+                {
+                    return j;
+                }
+
+                var temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
